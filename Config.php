9e5333d15c78729f2812308afa676e9f9eb40a15
Joey Sabey <GameFreak7744@gmail.com>
2017-03-26 19:03:04 +0100

    Added short draft Config and DB classes for shared functionality

---
+++ Config.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHlite;
+
+class Config {
+    protected const DEFAULT_FILE = 'phlite.cfg';
+
+    protected static $data = NULL;
+
+    public static function load(string $file = self::DEFAULT_FILE) : void {
+        $s = file_get_contents($file, FILE_USE_INCLUDE_PATH);
+        self::$data = parse_ini_string($s, true, INI_SCANNER_TYPED);
+    }
+
+    public static function get(string $section, string $param) {
+        if(self::$data === NULL)
+            self::load();
+        return self::$data[$section][$param];
+    }
+}
+
+?>
---
+++ DB.php
@@ -0,0 +1,34 @@
+<?php
+namespace PHlite;
+
+require_once "Config.php";
+
+use PDO;
+
+class DB {
+    protected static $pdo = NULL;
+
+    public static function get() : PDO {
+        self::connect();
+        return self::$pdo;
+    }
+
+    public static function exec(string $sql) : int {
+        return self::get()->exec($sql);
+    }
+
+    public static function execFile(string $f) : int {
+        $sql = file_get_contents($f, FILE_USE_INCLUDE_PATH);
+        return self::exec($sql);
+    }
+
+    protected static function connect() : void {
+        if(self::$pdo !== NULL)
+            return;
+        self::$pdo = new PDO('sqlite:'.Config::get('database', 'path'));
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::$pdo->exec('PRAGMA foreign_keys = ON');
+    }
+}
+
+?>
