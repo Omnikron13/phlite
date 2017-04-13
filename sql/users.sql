d9a064a6c825dac6db54cf9dd47a2d8942c8ff53
Joey Sabey <GameFreak7744@gmail.com>
2017-04-13 14:25:31 +0100

    username & email fields are now correctly unique in users table schema

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id           INTEGER PRIMARY KEY,
-    username     TEXT    NOT NULL COLLATE NOCASE,
-    email        TEXT    NOT NULL COLLATE NOCASE,
+    username     TEXT    NOT NULL UNIQUE COLLATE NOCASE,
+    email        TEXT    NOT NULL UNIQUE COLLATE NOCASE,
     password     TEXT    NOT NULL,
     registerTime INTEGER NOT NULL,
     failureCount INTEGER,
