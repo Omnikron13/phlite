74811cb58e40b9bb1ed3b27355f546ad95fcff3b
Joey Sabey <GameFreak7744@gmail.com>
2017-04-13 13:03:26 +0100

    Added users_logins_fail_view schema

--- sql/users_logins.sql
+++ sql/users_logins.sql
@@ -7,4 +7,7 @@ CREATE TABLE IF NOT EXISTS users_logins(
     FOREIGN KEY (userID) REFERENCES users(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-)
+);
+
+CREATE VIEW IF NOT EXISTS users_logins_fail_view AS
+    SELECT id, userID, time, IP FROM users_logins WHERE success = 0 ORDER BY time ASC;
