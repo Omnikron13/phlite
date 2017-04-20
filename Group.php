5558a2b8a596dc7a991e0c7d15046088d0935e15
Joey Sabey <GameFreak7744@gmail.com>
2017-04-20 16:17:48 +0100

    Added skeleton of Group class

---
+++ Group.php
@@ -0,0 +1,36 @@
+<?php
+namespace PHlite;
+
+use PDO;
+
+require_once 'DB.php';
+
+class Group {
+    protected $id          = NULL;
+    protected $name        = NULL;
+    protected $description = NULL;
+
+    public function __construct(int $id) {
+        $sql = 'SELECT * FROM groups WHERE id = :i';
+        $q = DB::prepare($sql);
+        $q->bindValue(':i', $i, PDO::PARAM_INT);
+        $q->execute();
+        $q->bindColumn('id',          $this->id,          PDO::PARAM_INT);
+        $q->bindColumn('name',        $this->name,        PDO::PARAM_STR);
+        $q->bindColumn('description', $this->description, PDO::PARAM_STR);
+        $q->fetch(PDO::FETCH_BOUND);
+        //TODO: throw on failure
+    }
+
+    public static function add(string $n, ?string $d = NULL) : self {
+        //TODO: validation
+        $sql = 'INSERT INTO groups(name, description) VALUES(:n, :d)';
+        $q = DB::prepare($sql);
+        $q->bindValue(':n', $n, PDO::PARAM_STR);
+        $q->bindValue(':d', $d, PDO::PARAM_STR);
+        $q->execute();
+        return new self(DB::get()->lastInsertId());
+    }
+}
+
+?>
