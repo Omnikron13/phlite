81257435ca8a78e375a0ecdcfe1a444a33567e47
Joey Sabey <GameFreak7744@gmail.com>
2017-05-10 11:45:12 +0100

    Added CODE constants to UserException

--- UserException.php
+++ UserException.php
@@ -2,6 +2,14 @@
 namespace Phlite;
 
 class UserException extends PhliteException {
+    public const CODE = [
+        'USER_NOT_FOUND'       => 1,
+        'USERNAME_INVALID'     => 2,
+        'USERNAME_UNAVAILABLE' => 3,
+        'EMAIL_INVALID'        => 4,
+        'EMAIL_UNAVAILABLE'    => 5,
+        'PASSWORD_INVALID'     => 6,
+    ];
 }
 
 ?>
