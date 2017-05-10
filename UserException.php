cbe8187969717f1018f251376427ddb21211b2b5
Joey Sabey <GameFreak7744@gmail.com>
2017-05-10 13:36:14 +0100

    Added code/message lookup array constant to UserException

--- UserException.php
+++ UserException.php
@@ -13,6 +13,14 @@ class UserException extends PhliteException {
         'EMAIL_UNAVAILABLE'    => 5,
         'PASSWORD_INVALID'     => 6,
     ];
+    protected const MESSAGE = [
+        self::CODE['USER_NOT_FOUND']       => 'User not found',
+        self::CODE['USERNAME_INVALID']     => 'Invalid username',
+        self::CODE['USERNAME_UNAVAILABLE'] => 'Unavailable username',
+        self::CODE['EMAIL_INVALID']        => 'Invalid email address',
+        self::CODE['EMAIL_UNAVAILABLE']    => 'Unavailable email address',
+        self::CODE['PASSWORD_INVALID']     => 'Invalid password',
+    ];
 
     public function __construct(int $code) {
         parent::__construct(null, $code + self::CODE_PREFIX);
